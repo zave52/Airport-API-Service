328241f08c5dccdef265b6a400aa347ef21c49a2
zakhar <zakhar.savchyn.23@pnu.edu.ua>
2025-02-09 16:28:55 +0200

    add media urls

--- airport_service/urls.py
+++ airport_service/urls.py
@@ -14,7 +14,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from drf_spectacular.views import (
@@ -26,7 +27,10 @@ from drf_spectacular.views import (
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("__debug__/", include("debug_toolbar.urls")),
-    path("api/v1/airport/", include("airport.urls", namespace="airport")),
+    path(
+        "api/v1/airport/",
+        include("airport.urls", namespace="airport")
+    ),
     path("api/v1/user/", include("user.urls", namespace="user")),
     path("api/v1/schema/", SpectacularAPIView.as_view(), name="schema"),
     path(
@@ -39,4 +43,4 @@ urlpatterns = [
         SpectacularRedocView.as_view(url_name="schema"),
         name="redoc"
     ),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
