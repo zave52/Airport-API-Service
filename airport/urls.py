f4c3a5fc81a7bf700b75031552cfeca962c77197
zakhar <zakhar.savchyn.23@pnu.edu.ua>
2025-02-10 18:04:07 +0200

    register airplane_types, airplanes, airports urls

--- airport/urls.py
+++ airport/urls.py
@@ -1,7 +1,16 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
+
+from airport.views import AirplaneTypeViewSet, AirplaneViewSet, AirportViewSet
+
+router = routers.DefaultRouter()
+
+router.register("airplane_types", AirplaneTypeViewSet)
+router.register("airplanes", AirplaneViewSet)
+router.register("airports", AirportViewSet)
 
 urlpatterns = [
-
+    path("", include(router.urls))
 ]
 
 app_name = "airport"
