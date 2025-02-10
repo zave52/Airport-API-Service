ff90761778e1cd5e51223a9948f90c6bb979ebcb
zakhar <zakhar.savchyn.23@pnu.edu.ua>
2025-02-10 20:51:02 +0200

    register flight and orders in urls, fix issue with wrong viewset in routes and crews

--- airport/urls.py
+++ airport/urls.py
@@ -1,15 +1,22 @@
 from django.urls import path, include
 from rest_framework import routers
 
-from airport.views import AirplaneTypeViewSet, AirplaneViewSet, AirportViewSet
+from airport.views import (
+    AirplaneTypeViewSet,
+    AirplaneViewSet,
+    AirportViewSet,
+    RouteViewSet, CrewViewSet, FlightViewSet, OrderViewSet
+)
 
 router = routers.DefaultRouter()
 
 router.register("airplane_types", AirplaneTypeViewSet)
 router.register("airplanes", AirplaneViewSet)
 router.register("airports", AirportViewSet)
-router.register("routes", AirportViewSet)
-router.register("crews", AirportViewSet)
+router.register("routes", RouteViewSet)
+router.register("crews", CrewViewSet)
+router.register("flights", FlightViewSet)
+router.register("orders", OrderViewSet)
 
 urlpatterns = [
     path("", include(router.urls))
