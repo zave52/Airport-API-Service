761e1c60d78ab138dbee5be4b13e520385a77b9f
zakhar <zakhar.savchyn.23@pnu.edu.ua>
2025-02-11 22:11:42 +0200

    add 0.0.0.0 to allowed hosts

--- airport_service/settings.py
+++ airport_service/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DJANGO_DEBUG", False)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["0.0.0.0"]
 
 INTERNAL_IPS = ["127.0.0.1"]
 
