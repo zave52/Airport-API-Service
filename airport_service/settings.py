d5b7b2b5ba2b3d3e5f603cfa12ab53b2a456c686
zakhar <zakhar.savchyn.23@pnu.edu.ua>
2025-02-11 21:44:38 +0200

    add media root and url, static root

--- airport_service/settings.py
+++ airport_service/settings.py
@@ -122,6 +122,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = "/vol/web/static"
+
+MEDIA_URL = "/media/"
+MEDIA_ROOT = "/vol/web/media"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
