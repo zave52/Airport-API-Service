71208afaea65e578f4dd6d538eb262f5d23ea4bb
zakhar <zakhar.savchyn.23@pnu.edu.ua>
2025-02-12 08:42:27 +0200

    add description for documentation

--- airport/views.py
+++ airport/views.py
@@ -1,5 +1,7 @@
 from django.db.models import QuerySet
 from django.http import HttpResponse, HttpRequest
+from drf_spectacular.types import OpenApiTypes
+from drf_spectacular.utils import extend_schema, OpenApiParameter
 from rest_framework import viewsets, mixins, serializers, status
 from rest_framework.decorators import action
 from rest_framework.permissions import IsAdminUser
@@ -43,6 +45,31 @@ class AirplaneTypeViewSet(
     queryset = AirplaneType.objects.all()
     serializer_class = AirplaneTypeSerializer
 
+    def list(self, request: HttpRequest, *args, **kwargs) -> HttpResponse:
+        """Get a list of all airplane types."""
+        return super().list(request, *args, **kwargs)
+
+    def create(self, request: HttpRequest, *args, **kwargs) -> HttpResponse:
+        """Create a new airplane type."""
+        return super().create(request, *args, **kwargs)
+
+    def update(self, request: HttpRequest, *args, **kwargs) -> HttpResponse:
+        """Update an existing airplane type."""
+        return super().update(request, *args, **kwargs)
+
+    def retrieve(self, request: HttpRequest, *args, **kwargs) -> HttpResponse:
+        """Retrieve details of a specific airplane type."""
+        return super().retrieve(request, *args, **kwargs)
+
+    def partial_update(
+        self,
+        request: HttpRequest,
+        *args,
+        **kwargs
+    ) -> HttpResponse:
+        """Partially update an airplane type."""
+        return super().partial_update(request, *args, **kwargs)
+
 
 class AirplaneViewSet(
     mixins.ListModelMixin,
@@ -80,6 +107,21 @@ class AirplaneViewSet(
             return AirplaneImageSerializer
         return self.serializer_class
 
+    @extend_schema(
+        request={
+            "multipart/form-data": {
+                "type": "object",
+                "properties": {
+                    "image": {
+                        "type": "string",
+                        "format": "binary",
+                        "description": "Image file to upload"
+                    }
+                },
+                "required": ["image"]
+            }
+        }
+    )
     @action(
         methods=("post",),
         detail=True,
@@ -91,11 +133,50 @@ class AirplaneViewSet(
         request: HttpRequest,
         pk: int = None
     ) -> HttpResponse:
+        """
+        Upload an image for a specific airplane.
+        Only accessible by admin users.
+        """
         airplane = self.get_object()
         serializer = self.get_serializer(airplane, data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data, status=status.HTTP_200_OK)
+
+    @extend_schema(
+        parameters=[
+            OpenApiParameter(
+                "airplane_types",
+                type={"type": "list", "items": {"type": "integer"}},
+                description="Filter airplanes by their type IDs "
+                            "(ex. ?airplane_types=1,2)."
+            )
+        ]
+    )
+    def list(self, request: HttpRequest, *args, **kwargs) -> HttpResponse:
+        """Get a list of all airplanes."""
+        return super().list(request, *args, **kwargs)
+
+    def create(self, request: HttpRequest, *args, **kwargs) -> HttpResponse:
+        """Create a new airplane."""
+        return super().create(request, *args, **kwargs)
+
+    def update(self, request: HttpRequest, *args, **kwargs) -> HttpResponse:
+        """Update an existing airplane."""
+        return super().update(request, *args, **kwargs)
+
+    def retrieve(self, request: HttpRequest, *args, **kwargs) -> HttpResponse:
+        """Retrieve details of a specific airplane."""
+        return super().retrieve(request, *args, **kwargs)
+
+    def partial_update(
+        self,
+        request: HttpRequest,
+        *args,
+        **kwargs
+    ) -> HttpResponse:
+        """Partially update an airplane."""
+        return super().partial_update(request, *args, **kwargs)
 
 
 class AirportViewSet(
@@ -107,6 +188,31 @@ class AirportViewSet(
 ):
     queryset = Airport.objects.all()
     serializer_class = AirportSerializer
+
+    def list(self, request: HttpRequest, *args, **kwargs) -> HttpResponse:
+        """Get a list of all airports."""
+        return super().list(request, *args, **kwargs)
+
+    def create(self, request: HttpRequest, *args, **kwargs) -> HttpResponse:
+        """Create a new airport."""
+        return super().create(request, *args, **kwargs)
+
+    def update(self, request: HttpRequest, *args, **kwargs) -> HttpResponse:
+        """Update an existing airport."""
+        return super().update(request, *args, **kwargs)
+
+    def retrieve(self, request: HttpRequest, *args, **kwargs) -> HttpResponse:
+        """Retrieve details of a specific airport."""
+        return super().retrieve(request, *args, **kwargs)
+
+    def partial_update(
+        self,
+        request: HttpRequest,
+        *args,
+        **kwargs
+    ) -> HttpResponse:
+        """Partially update an airport."""
+        return super().partial_update(request, *args, **kwargs)
 
 
 class RouteViewSet(
@@ -147,6 +253,48 @@ class RouteViewSet(
             return RouteRetrieveSerializer
         return self.serializer_class
 
+    @extend_schema(
+        parameters=[
+
+            OpenApiParameter(
+                "source",
+                type=OpenApiTypes.STR,
+                description="Filter routes by source airport's "
+                            "closest big city (ex. ?source=Kyiv)."
+            ),
+            OpenApiParameter(
+                "destination",
+                type=OpenApiTypes.STR,
+                description="Filter routes by destination airport's closest "
+                            "big city (ex. ?destination=Lviv)."
+            )
+        ]
+    )
+    def list(self, request: HttpRequest, *args, **kwargs) -> HttpResponse:
+        """Get a list of all routes."""
+        return super().list(request, *args, **kwargs)
+
+    def create(self, request: HttpRequest, *args, **kwargs) -> HttpResponse:
+        """Create a new route."""
+        return super().create(request, *args, **kwargs)
+
+    def update(self, request: HttpRequest, *args, **kwargs) -> HttpResponse:
+        """Update an existing route."""
+        return super().update(request, *args, **kwargs)
+
+    def retrieve(self, request: HttpRequest, *args, **kwargs) -> HttpResponse:
+        """Retrieve details of a specific route."""
+        return super().retrieve(request, *args, **kwargs)
+
+    def partial_update(
+        self,
+        request: HttpRequest,
+        *args,
+        **kwargs
+    ) -> HttpResponse:
+        """Partially update a route."""
+        return super().partial_update(request, *args, **kwargs)
+
 
 class CrewViewSet(
     mixins.ListModelMixin,
@@ -157,6 +305,31 @@ class CrewViewSet(
 ):
     queryset = Crew.objects.all()
     serializer_class = CrewSerializer
+
+    def list(self, request: HttpRequest, *args, **kwargs) -> HttpResponse:
+        """Get a list of all crews."""
+        return super().list(request, *args, **kwargs)
+
+    def create(self, request: HttpRequest, *args, **kwargs) -> HttpResponse:
+        """Create a new crew."""
+        return super().create(request, *args, **kwargs)
+
+    def update(self, request: HttpRequest, *args, **kwargs) -> HttpResponse:
+        """Update an existing crew."""
+        return super().update(request, *args, **kwargs)
+
+    def retrieve(self, request: HttpRequest, *args, **kwargs) -> HttpResponse:
+        """Retrieve details of a specific crew."""
+        return super().retrieve(request, *args, **kwargs)
+
+    def partial_update(
+        self,
+        request: HttpRequest,
+        *args,
+        **kwargs
+    ) -> HttpResponse:
+        """Partially update a crew."""
+        return super().partial_update(request, *args, **kwargs)
 
 
 class FlightViewSet(
@@ -194,6 +367,40 @@ class FlightViewSet(
             return FlightRetrieveSerializer
         return self.serializer_class
 
+    @extend_schema(
+        parameters=[
+            OpenApiParameter(
+                "route",
+                type=OpenApiTypes.STR,
+                description="Filter flights by route (ex. ?route=Kyiv-Lviv)."
+            )
+        ]
+    )
+    def list(self, request: HttpRequest, *args, **kwargs) -> HttpResponse:
+        """Get a list of all flights."""
+        return super().list(request, *args, **kwargs)
+
+    def create(self, request: HttpRequest, *args, **kwargs) -> HttpResponse:
+        """Create a new flight."""
+        return super().create(request, *args, **kwargs)
+
+    def update(self, request: HttpRequest, *args, **kwargs) -> HttpResponse:
+        """Update an existing flight."""
+        return super().update(request, *args, **kwargs)
+
+    def retrieve(self, request: HttpRequest, *args, **kwargs) -> HttpResponse:
+        """Retrieve details of a specific flight."""
+        return super().retrieve(request, *args, **kwargs)
+
+    def partial_update(
+        self,
+        request: HttpRequest,
+        *args,
+        **kwargs
+    ) -> HttpResponse:
+        """Partially update a flight."""
+        return super().partial_update(request, *args, **kwargs)
+
 
 class OrderViewSet(
     mixins.ListModelMixin,
@@ -223,3 +430,31 @@ class OrderViewSet(
         serializer: type(serializers.ModelSerializer)
     ) -> None:
         serializer.save(user=self.request.user)
+
+    def list(self, request: HttpRequest, *args, **kwargs) -> HttpResponse:
+        """Get a list of orders for the currently authenticated user."""
+        return super().list(request, *args, **kwargs)
+
+    def create(self, request: HttpRequest, *args, **kwargs) -> HttpResponse:
+        """Create a new order for the currently authenticated user."""
+        return super().create(request, *args, **kwargs)
+
+    def update(self, request: HttpRequest, *args, **kwargs) -> HttpResponse:
+        """Update an existing order for the currently authenticated user."""
+        return super().update(request, *args, **kwargs)
+
+    def retrieve(self, request: HttpRequest, *args, **kwargs) -> HttpResponse:
+        """
+        Retrieve details of a specific order for
+        the currently authenticated user.
+        """
+        return super().retrieve(request, *args, **kwargs)
+
+    def partial_update(
+        self,
+        request: HttpRequest,
+        *args,
+        **kwargs
+    ) -> HttpResponse:
+        """Partially update an order for the currently authenticated user."""
+        return super().partial_update(request, *args, **kwargs)
