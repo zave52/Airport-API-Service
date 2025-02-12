f8c5428b776bfb3ae75f3e993b8bb9483480f9c3
zakhar <zakhar.savchyn.23@pnu.edu.ua>
2025-02-12 09:00:53 +0200

    change logic of retrieve django_debug from environment variables

--- airport_service/settings.py
+++ airport_service/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get(
 )
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("DJANGO_DEBUG", False)
+DEBUG = os.environ.get("DJANGO_DEBUG", False).lower() in ("true", 1, "t")
 
 ALLOWED_HOSTS = ["0.0.0.0"]
 
