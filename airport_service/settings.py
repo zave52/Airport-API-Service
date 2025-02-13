6d0ea121ac181c19f967abc85c3e0e0f9817d6d1
zakhar <zakhar.savchyn.23@pnu.edu.ua>
2025-02-13 09:26:58 +0200

    change default value for secret key

--- airport_service/settings.py
+++ airport_service/settings.py
@@ -20,10 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get(
-    "SECRET_KEY",
-    "django-insecure-i9mvj7@76%9y3r$$wze(v1rxb^_!#w(_b^u=-wot=dd(218ay6"
-)
+SECRET_KEY = os.environ.get("SECRET_KEY", "default-secret-key-django")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DJANGO_DEBUG", False).lower() in ("true", 1, "t")
